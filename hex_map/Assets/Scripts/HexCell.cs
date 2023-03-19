92e03f905456d7744ac97280d0514e703a4527e0
Simon Beck <simubeck@hotmail.com>
2023-03-19 16:11:16 +0100

    blending cell colors

--- hex_map/Assets/Scripts/HexCell.cs
+++ hex_map/Assets/Scripts/HexCell.cs
@@ -4,5 +4,20 @@ public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
 
+    [SerializeField]
+    public HexCell[] neighbors;
+
     public Color color;
+
+
+    public HexCell GetNeighbor (HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
 }
